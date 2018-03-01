97e0e6e24e00cbce2e993a0ef06d9be528112938
DK\obr <obr@sitecore.net>
2018-03-01 10:19:53 +0200

    Init

---
+++ src/Sitecore.Support.209652/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.209652")]
+[assembly: AssemblyProduct("Sitecore.Support.209652")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.209652/Resources/Media/RestoreColorProfileProcessor.cs
@@ -0,0 +1,44 @@
+﻿namespace Sitecore.Support.Resources.Media
+{
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using Sitecore.Diagnostics;
+    using Sitecore.Resources.Media;
+
+    /// <summary>
+    /// RestoreColorProfileProcessor class
+    /// </summary>
+    public class RestoreColorProfileProcessor
+    {
+        /// <summary>
+        /// Runs the processor.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public void Process([NotNull] GetMediaStreamPipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+
+            if (!(args.CustomData["ColorProfile"] is PropertyItem) || args.OutputStream == null)
+            {
+                return;
+            }
+
+            ImageFormat imageFormat = MediaManager.Config.GetImageFormat(args.MediaData.Extension, null);
+            if (imageFormat == null)
+            {
+                return;
+            }
+
+            using (var stream = args.OutputStream.Stream)
+            {
+                var bitmap = new Bitmap(stream);
+                bitmap.SetPropertyItem(args.CustomData["ColorProfile"] as PropertyItem);
+
+                var memoryStream = new MemoryStream();
+                bitmap.Save(memoryStream, bitmap.RawFormat);                
+                args.OutputStream = new MediaStream(memoryStream, args.MediaData.Extension, args.MediaData.MediaItem);
+            }
+        }
+    }
+}
